cf0b53f7ddac9dc3b86c8a4c521abf4bcc356981
yaakov111 <yaakovmargalit@gmail.com>
2022-09-21 09:23:01 +0300

    dddd

--- src/app/components/questions-box/questions-box.component.ts
+++ src/app/components/questions-box/questions-box.component.ts
@@ -10,22 +10,42 @@ import { QuestionsService } from 'src/app/service/questions.service';
 export class QuestionsBoxComponent implements OnInit {
   constructor(private questionsService: QuestionsService) {}
   questions$: Observable<any> = this.questionsService.getQuestion();
-  answers:any
-  question!:any
+  answers: any;
+  question!: any;
   questionNum: number = 1;
   ngOnInit(): void {}
-  findQuestion = async() => {
-  await  this.questionsService.setQuestions(this.questionNum)
+  findQuestion = async () => {
+    await this.questionsService.setQuestions(this.questionNum);
 
-  this.questions$.subscribe(val=>{
-    let data =val[0]
-    // data = data.replaceAll('<span>','')
-    // data = data.replaceAll('</span>','')
-    this.question =data["title"]
-    this.answers =data["description"]
-    document.querySelector("#answers-text")!.innerHTML=this.answers
-    document.querySelector("#question-text")!.innerHTML=this.question
-  })
-
+    this.questions$.subscribe((val) => {
+      let data = val[0];
+      this.question = data['title'];
+      this.question = this.question.slice(6,this.question.length)
+      console.log(this.question);
+      
+      this.answers = data['description'];
+      document.querySelector('#answers-text')!.innerHTML = this.answers;
+      document.querySelector('#question-text')!.innerHTML = this.question;
+      const start = this.answers.indexOf('<ul>');
+      const end = this.answers.indexOf('</ul>');
+      let ul = this.answers.slice(start + 4, end);
+      const ansArray = [];
+      for (let i = 0; i < 4; i++) {
+        const startLi = ul.indexOf('<li>');
+        const endLi = ul.indexOf('</li>');
+        let ans = ul
+          .slice(startLi + 4, endLi)
+          .replaceAll('  ', '')
+          .replaceAll('<span>', '')
+          .replaceAll('</span>', '');
+        if (ans.indexOf('<span') !== -1) {
+          console.log("correct ans = ",i + 1);
+          ans= ans.slice(ans.indexOf(">")+1,ans.length)
+        }
+        ansArray.push(ans);
+        ul = ul.slice(endLi + 4, ul.length);
+      }
+      console.log(ansArray);
+    });
   };
 }
